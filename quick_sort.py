dcf97ed8150f778d8cf87e6dd192ede1eb3c1c75
Roman Abaghayn <84144854+777ARM777@users.noreply.github.com>
2023-11-21 21:47:42 +0400

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -4,6 +4,8 @@ import random
 def quick_sort(arr, start, end):
     if start < end:
         pivot = partition_random_pivot(arr, start, end)
+        # pivot = partition_start_pivot(arr, start, end)
+        # pivot = partition_end_pivot(arr, start, end)
         quick_sort(arr, start, pivot - 1)
         quick_sort(arr, pivot + 1, end)
 
@@ -34,11 +36,8 @@ def partition_random_pivot(arr, start, end):
     pivot = random.randint(start, end)
     arr[pivot], arr[end] = arr[end], arr[pivot]
     index = start - 1
-    print(arr)
-    print(pivot)
-    print(arr[start + 1:end + 1])
     for i in range(start, end):
-        if arr[i] < arr[pivot]:
+        if arr[i] < arr[end]:
             index += 1
             arr[i], arr[index] = arr[index], arr[i]
     arr[index + 1], arr[end] = arr[end], arr[index + 1]
@@ -46,6 +45,7 @@ def partition_random_pivot(arr, start, end):
 
 
 lst = [5, 8, 9, 7, 4, 6, 5, 10, 0, 8, 45, 15, 31, 9]
-# for i in range(50):
-quick_sort(lst, 0, len(lst) - 1)
-print(lst)
+for i in range(50):
+    quick_sort(lst, 0, len(lst) - 1)
+    print(lst)
+
